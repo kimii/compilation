88d323dffe5c8c862d19a866f176837b460789a1
johnsonyue <johnsonyuehit@163.com>
2018-03-14 15:49:52 +0000

    fixed forced layout first time update problem

--- app/web/src/library.js
+++ app/web/src/library.js
@@ -219,27 +219,30 @@ export function myD3Graph(container, data, options){
     .force("charge", d3.forceManyBody())
     .force("center", d3.forceCenter(width/2, height/2));
   
-  redraw();
   window.addEventListener("resize", resized);
 
-  function redraw(){
-    svg.selectAll(".nodes").remove();
-    svg.selectAll(".links").remove();
-    
-    var link = svg.append("g")
-      .attr("class","links")
-    .selectAll("line")
-      .data(data.links)
-      .enter().append("line")
+  var links = svg.append("g")
+    .attr("class","links");
+  var nodes = svg.append("g")
+    .attr("class","nodes");
+  var link, node;
+  
+  update();
+  link = links.selectAll("line").data(data.links);
+  node = nodes.selectAll("circle").data(data.nodes); //BUG??
+
+  function update(){
+    link = links.selectAll("line")
+      .data(data.links);
+    link.enter().append("line")
       .style("stroke", function(d){return "#3498db";})
       .style("opacity", 0.7)
       .style("stroke-width", 1);
+    link.exit().remove();
     
-    var node = svg.append("g")
-      .attr("class","nodes")
-    .selectAll("circle")
-      .data(data.nodes)
-      .enter().append("circle")
+    node = nodes.selectAll("circle")
+      .data(data.nodes);
+    node.enter().append("circle")
       .attr("r", 5)
       .attr("fill", "#3498db")
       .attr("id", function(d){return d.id;})
@@ -247,24 +250,28 @@ export function myD3Graph(container, data, options){
         .on("start", dragstarted)
         .on("drag", dragged)
         .on("end", dragended));
-    
+    node.exit().remove();
+    node.append("title")
+      .text(function(d){return d.id;});
+
     simulation
       .nodes(data.nodes)
       .on("tick", ticked);
     simulation.force("link")
       .links(data.links);
-
-    function ticked(){
-      link.attr("x1", function(d) { return d.source.x; })
-        .attr("y1", function(d) { return d.source.y; })
-        .attr("x2", function(d) { return d.target.x; })
-        .attr("y2", function(d) { return d.target.y; });
-
-      node.attr("cx", function(d) { return d.x; })
-        .attr("cy", function(d) { return d.y; });
-    }
+    simulation.restart();
   }
-  
+
+  function ticked(){
+    link.attr("x1", function(d) { return d.source.x; })
+      .attr("y1", function(d) { return d.source.y; })
+      .attr("x2", function(d) { return d.target.x; })
+      .attr("y2", function(d) { return d.target.y; });
+
+    node.attr("cx", function(d) { return d.x; })
+      .attr("cy", function(d) { return d.y; });
+  }
+
   function resized(){
     width = $(container).width();
     height = $(container).height();
@@ -291,6 +298,10 @@ export function myD3Graph(container, data, options){
     d.fx = null;
     d.fy = null;
   }
+  
+  return {
+    "update": update
+  };
 
 }
 
